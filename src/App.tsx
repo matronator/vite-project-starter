438989fe1f6d4446551704653c305861f4607d6a
Matronator <5470780+matronator@users.noreply.github.com>
2024-01-20 03:46:39 +0100

    remove bs, add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
         <div className="app">
             <div className="container">
                 <h1>vite-project-starter</h1>
-                <h3>Vite + pnpm + TypeScript + React + PostCSS + Bootstrap</h3>
+                <h3>Vite + pnpm + TypeScript + React + PostCSS + Mantine</h3>
                 <h4>By <a href="https://matronator.com" target="_blank" rel="noopener">Matronator</a></h4>
             </div>
         </div>
